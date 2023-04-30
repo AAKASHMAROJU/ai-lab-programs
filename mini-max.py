7c694b6e923403caa4227fc80f944569d44fdc06
aakash <marojuaakash2003@gmail.com>
2023-04-30 13:42:01 +0530

    commit changes 1

--- mini-max.py
+++ mini-max.py
@@ -1,1 +1,15 @@
-# 
+# Mini Max Algorithm 
+import math
+
+def minimax(currDepth,currIdx,maxTurn,scores,targetDepth):
+    if currDepth == targetDepth:
+        return scores[currIdx]
+    if maxTurn:
+        return max(minimax(currDepth+1,2*currIdx,False,scores,targetDepth),minimax(currDepth+1,2*currIdx+1,False,scores,targetDepth))
+    else:
+        return min((minimax(currDepth+1,2*currIdx,True,scores,targetDepth),minimax(currDepth+1,2*currIdx+1,True,scores,targetDepth)))
+
+print("Enter the Elements : ")
+scores = list(map(int,input().split()))
+targetDepth=math.log(len(scores),2)
+print(minimax(0,0,True,scores,targetDepth))
